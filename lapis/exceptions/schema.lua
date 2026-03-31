b5c0aa33f9bcdcae2b361100686316a4f3a1b84b
leaf corcoran <leafot@gmail.com>
2026-03-31 11:41:35 -0700

    add migrations version check (not idea with timestamps, but better than nothing)

--- lapis/exceptions/schema.lua
+++ lapis/exceptions/schema.lua
@@ -1,7 +1,23 @@
 local run_migrations
-run_migrations = function()
+run_migrations = function(version)
   local m = require("lapis.db.migrations")
-  return m.run_migrations(require("lapis.exceptions.migrations"), "lapis_exceptions")
+  local migrations = require("lapis.exceptions.migrations")
+  if version and not migrations[tonumber(version)] then
+    local versions
+    do
+      local _accum_0 = { }
+      local _len_0 = 1
+      for key in pairs(migrations) do
+        _accum_0[_len_0] = key
+        _len_0 = _len_0 + 1
+      end
+      versions = _accum_0
+    end
+    table.sort(versions)
+    local available_version = versions[#versions]
+    error("Expected to migrate to lapis-exceptions version " .. tostring(version) .. " but it was not found (have " .. tostring(available_version) .. "). Did you forget to update lapis-exceptions?")
+  end
+  return m.run_migrations(migrations, "lapis_exceptions")
 end
 return {
   run_migrations = run_migrations
--- lapis/exceptions/schema.moon
+++ lapis/exceptions/schema.moon
@@ -1,5 +1,14 @@
-run_migrations = ->
+run_migrations = (version) ->
   m = require "lapis.db.migrations"
-  m.run_migrations require("lapis.exceptions.migrations"), "lapis_exceptions"
+
+  migrations = require("lapis.exceptions.migrations")
+
+  if version and not migrations[tonumber version]
+    versions = [key for key in pairs migrations]
+    table.sort versions
+    available_version = versions[#versions]
+    error "Expected to migrate to lapis-exceptions version #{version} but it was not found (have #{available_version}). Did you forget to update lapis-exceptions?"
+
+  m.run_migrations migrations, "lapis_exceptions"
 
 { :run_migrations }
